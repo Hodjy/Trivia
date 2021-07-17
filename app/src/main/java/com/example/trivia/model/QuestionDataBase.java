8243cc93826f0db6a2316378d477593d9ec0d753
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2021-07-18 00:45:48 +0300

    Updated easy mode database

--- app/src/main/java/com/example/trivia/model/QuestionDataBase.java
+++ app/src/main/java/com/example/trivia/model/QuestionDataBase.java
@@ -36,6 +36,54 @@ public final class QuestionDataBase {
                 new Answer("Aba", false),
                 new Answer("Aba", false)));
 
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/anemone"),
+                new Answer("Aba", false),
+                new Answer("anemone", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/bleeding_heart"),
+                new Answer("Aba", false),
+                new Answer("bleeding_heart", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/iris"),
+                new Answer("Aba", false),
+                new Answer("iris", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/jasmine"),
+                new Answer("Aba", false),
+                new Answer("jasmin", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/lilac"),
+                new Answer("Aba", false),
+                new Answer("lilac", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/lotus"),
+                new Answer("Aba", false),
+                new Answer("lotus", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/narcissus"),
+                new Answer("Aba", false),
+                new Answer("narcissus", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
+        easyQuestions.add(new Question(Uri.parse("android.resource://com.example.trivia/drawable/orchid"),
+                new Answer("Aba", false),
+                new Answer("orchid", true),
+                new Answer("Aba", false),
+                new Answer("Aba", false)));
+
         return easyQuestions;
     }
 
