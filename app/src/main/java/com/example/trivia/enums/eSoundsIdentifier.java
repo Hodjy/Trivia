dbedc037673234436160c8c5d19c2ac61f751513
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2021-07-18 20:26:56 +0300

    Added SoundsManager Class and SoundsIdentifier enum

---
+++ app/src/main/java/com/example/trivia/enums/eSoundsIdentifier.java
@@ -0,0 +1,13 @@
+package com.example.trivia.enums;
+
+public enum eSoundsIdentifier {
+    //QUESTIONNAIRE_NEW_PATIENT("questionnaire_new_patient"),
+    //QUESTIONNAIRE_FOLLOW_UP("questionnaire_follow_up"),
+    //EDICINE_LIST("medicine_list");
+    ;
+    public final int id;
+
+    private eSoundsIdentifier(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/trivia/model/SoundManager.java
@@ -0,0 +1,40 @@
+package com.example.trivia.model;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import com.example.trivia.enums.eSoundsIdentifier;
+
+public class SoundManager {
+
+    private MediaPlayer m_MediaPlayer;
+    private static SoundManager m_Instance;
+
+    private SoundManager(){}
+
+    public static SoundManager getInstance(){
+        if(m_Instance == null)
+            m_Instance = new SoundManager();
+        return m_Instance;
+    }
+
+    public void playSound(Context context, eSoundsIdentifier soundIdentifier){
+        if(m_MediaPlayer != null)
+        {
+            m_MediaPlayer.release();
+        }
+        m_MediaPlayer = MediaPlayer.create(context, soundIdentifier.id);
+        m_MediaPlayer.start();
+    }
+
+    public void pauseSound(){
+        if(m_MediaPlayer != null){
+            m_MediaPlayer.pause();
+        }
+    }
+
+    public void resumeSound(){
+        if(m_MediaPlayer != null){
+            m_MediaPlayer.start();
+        }
+    }
+}
