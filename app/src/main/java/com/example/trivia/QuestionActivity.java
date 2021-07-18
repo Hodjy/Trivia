911ff48970caf3493dbe59a93f70bb422d4bcded
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2021-07-18 14:38:28 +0300

    Updated timer

--- app/src/main/java/com/example/trivia/QuestionActivity.java
+++ app/src/main/java/com/example/trivia/QuestionActivity.java
@@ -20,6 +20,8 @@ import com.example.trivia.model.Question;
 import com.example.trivia.model.QuestionDataBase;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class QuestionActivity extends AppCompatActivity
 {
@@ -30,11 +32,13 @@ public class QuestionActivity extends AppCompatActivity
     private AnswerButton m_Btn4;
     private TextView m_LivesTv;
     private TextView m_ScoreTv;
+
     private TextView m_TimerCounterTv;
     private Runnable m_Timer_Tick;
     private int m_SecondsLeft = 15;
+    private final int m_QuestionSecounds = 15;
+    private Timer m_gameTimer;
 
-    private ArrayList<Question> m_Questions;
     private GameSessionManager m_GameSessionManager;
     private GameState m_GameState;
 
@@ -53,7 +57,8 @@ public class QuestionActivity extends AppCompatActivity
         m_ScoreTv = findViewById(R.id.questionActivity_scoreTV);
         m_TimerCounterTv = findViewById(R.id.level_counter_time_view);
 
-        m_Questions = new ArrayList<>();
+        setTimerTick();
+        setTimer();
 
         View.OnClickListener answerListener =  new View.OnClickListener()
         {
@@ -117,7 +122,19 @@ public class QuestionActivity extends AppCompatActivity
 
     private void endGame()
     {
+        m_TimerCounterTv.setBackground(getResources().getDrawable(R.drawable.timer_backgraund, this.getApplication().getTheme()));
         Toast.makeText(this, "Game Ended", Toast.LENGTH_LONG).show();
+    }
+
+    private void setTimer() {
+        m_gameTimer = new Timer();
+        m_gameTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                TimerMethod();
+            }
+
+        }, 0, 1000);
     }
 
     private void setTimerTick() {
@@ -127,64 +144,42 @@ public class QuestionActivity extends AppCompatActivity
                 m_SecondsLeft--;
                 m_TimerCounterTv.setText(m_SecondsLeft +"");
 
-                if(m_SecondsLeft <= 10 && m_SecondsLeft>0)
+                if(m_SecondsLeft <= 5 && m_SecondsLeft>0)
                 {
-                    TimerTickingSound();
+                    //TimerTickingSound();
                     if(m_SecondsLeft ==5)
                         TimeTikcingEffect();
                 }
 
-                if(m_secondsLeft == 0) {
-                    TimeUpSound();
+                if(m_SecondsLeft == 0) {
+                    //TimeUpSound();
                     m_gameTimer.cancel();
-                    m_lastRoundScore = m_Compleate_counter;
-                    setEnterNameVisabilty();
-                    m_TimeUpDialog.show();
-                    m_timerCounter_TextView.setBackground(getResources().getDrawable(R.drawable.timer_backgraund));
+                    endGame();
+                    //m_lastRoundScore = m_Compleate_counter;
+                    //m_TimeUpDialog.show();
                 }
             }
         };
     }
 
-    private void setEnterNameVisabilty() {
-        EditText enter_name_ET = m_TimeUpDialogView.findViewById(R.id.enter_name_high_score);
-        TextView top_ten_TV = m_TimeUpDialogView.findViewById(R.id.msg_in_top_10__txt);
-        TextView msg_how_many_completed_TV = m_TimeUpDialogView.findViewById(R.id.how_many_level_complete_msg_txt);
-        String msg = getString(R.string.msg_level_compleate_string)+m_lastRoundScore+" "+getString(R.string.levels_string);
-        msg_how_many_completed_TV.setText(msg);
-
-        if(gameManager.isInTopTen(m_Compleate_counter))
-        {
-            enter_name_ET.setVisibility(View.VISIBLE);
-            top_ten_TV.setVisibility(View.VISIBLE);
-            enter_name_ET.requestLayout();
-        }
-        else
-        {
-            enter_name_ET.setVisibility(View.GONE);
-            top_ten_TV.setVisibility(View.GONE);
-            enter_name_ET.requestLayout();
-        }
-    }
-
-    @SuppressLint("WrongConstant")
+    //@SuppressLint("WrongConstant")
     private void TimeTikcingEffect()
     {
-        new CountDownTimer(10000, 500) {
+        new CountDownTimer(5000, 500) {
             int count = 0;
             @Override
             public void onTick(long millisUntilFinished) {
                 if(count == 0) {
-                    m_timerCounter_TextView.setBackground(getResources().getDrawable(R.drawable.timer_red_backgraund));
+                    m_TimerCounterTv.setBackground(getResources().getDrawable(R.drawable.timer_red_backgraund, QuestionActivity.this.getApplication().getTheme()));
                     count = 1;
                 } else {
-                    m_timerCounter_TextView.setBackground(getResources().getDrawable(R.drawable.timer_red_stroke));
+                    m_TimerCounterTv.setBackground(getResources().getDrawable(R.drawable.timer_red_stroke,QuestionActivity.this.getApplication().getTheme()));
                     count = 0;
                 }
             }
             @Override
             public void onFinish() {
-                m_timerCounter_TextView.setBackground(getResources().getDrawable(R.drawable.timer_backgraund));
+                m_TimerCounterTv.setBackground(getResources().getDrawable(R.drawable.timer_backgraund, QuestionActivity.this.getApplication().getTheme()));
             }
         }.start();
     }
@@ -192,5 +187,10 @@ public class QuestionActivity extends AppCompatActivity
     private void TimerMethod()
     {
         this.runOnUiThread(m_Timer_Tick);
+    }
+
+    public void resetQuestionTimer()
+    {
+        m_SecondsLeft = m_QuestionSecounds;
     }
 }
