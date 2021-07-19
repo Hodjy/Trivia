b53c4c095d037c6dedc3e4c5b557220aae10fb8f
OfirKosto <59827963+OfirKosto@users.noreply.github.com>
2021-07-19 12:51:15 +0300

    Added sounds and started work on main activity

--- app/src/main/java/com/example/trivia/enums/eSoundsIdentifier.java
+++ app/src/main/java/com/example/trivia/enums/eSoundsIdentifier.java
@@ -4,7 +4,11 @@ import com.example.trivia.R;
 
 public enum eSoundsIdentifier {
     BTN_CLICK_SOUND(R.raw.btn_click_sound),
-    TIME_TICKING_SOUND(R.raw.time_ticking_sound);
+    TIME_TICKING_SOUND(R.raw.time_ticking_sound),
+    CORRECT_ANSWER_SOUND(R.raw.correct_answer_sound),
+    INCORRECT_ANSWER_SOUND(R.raw.incorrect_answer_sound),
+    SCORE_UP_SOUND(R.raw.score_up_sound),
+    END_GAME_SOUND(R.raw.end_game_sound);
 
     public final int id;
 
